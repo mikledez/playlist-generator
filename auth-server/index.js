0ae7f7c8e23ec4dfcaa0fab53738fe8e099d49af
mikledez <miwork546@gmail.com>
2026-05-04 16:39:09 +0800

    Add groq

--- auth-server/index.js
+++ auth-server/index.js
@@ -1,14 +1,17 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 import authRoutes from "./routes/auth.js";
 import spotifyRoutes from "./routes/spotify.js";
 
-dotenv.config();
+import Groq from "groq-sdk";
 
 const app = express();
+
 app.use(cors({
     origin: "http://127.0.0.1:5173",
     credentials: true
@@ -20,5 +23,30 @@ const PORT = 5000;
 
 app.use("/", authRoutes);
 app.use("/spotify", spotifyRoutes);
+
+
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+export async function main() {
+    const chatCompletion = await getGroqChatCompletion();
+    // Print the completion returned by the LLM.
+    console.log(chatCompletion.choices[0]?.message?.content || "");
+}
+
+export async function getGroqChatCompletion() {
+    return groq.chat.completions.create({
+        messages: [
+            {
+                role: "user",
+                content: "hello",
+            },
+        ],
+        model: "openai/gpt-oss-20b",
+    });
+}
+
+main();
+
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
